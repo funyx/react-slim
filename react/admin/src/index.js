46efcb76debc45dbb518233f4966059d87c0394e
Dimitar Dobchev <ddobchev@gmail.com>
2021-09-09 12:25:45 +0300

    protected admin dashboard setup

--- react/admin/src/index.js
+++ react/admin/src/index.js
@@ -1,6 +1,8 @@
+import './loader';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
